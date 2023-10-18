a0e3a557ecbe1b05e6c3755701343a9c9a7a00a3
Fasca-kcct <r121317@g.kobe-kosen.ac.jp>
2023-10-18 20:34:44 +0900

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +14,5 @@ export default defineConfig({
   }), compress(), react()],
   site: 'https://hayatann.net',
   output: 'static',
+  adapter: vercel()
 });
