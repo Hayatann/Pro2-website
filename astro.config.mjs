5a6fd18d1ab551b0abe106b51a059dffce8c6130
Fasca-kcct <r121317@g.kobe-kosen.ac.jp>
2023-10-18 20:36:57 +0900

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import react from '@astrojs/react'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), tailwind({
-    applyBaseStyles: false
-  }), compress(), react()],
+  integrations: [
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+    react(),
+  ],
   site: 'https://hayatann.net',
-  output: 'static',
-  adapter: vercel()
-});
+  output: 'server',
+  adapter: vercel(),
+})
